afa075fb0333fabce264a8e55e740491580ddcdb
BrunoBonadiman <bruno.bonadiman@autoglass.com.br>
2021-01-22 15:07:29 -0300

    configura ambiente
    Configuração do ambiente para deploy da aplicação no heroku

---
+++ front/server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use(express.static('./dist/mdb-angular-free'));
+
+app.get('/*', (req, res) =>{
+  res.sendFile('index.html', {root: './dist/mdb-angular-free/'});
+});
+
+app.listen(process.env.PORT || 8080);
+})
